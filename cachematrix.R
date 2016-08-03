e7caa3af018702e26e49aab380402cfdae2193a1
chaudharyan <anisa.chaudhary@bucks.edu>
2016-08-03 11:20:59 -0400

    message

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+##############################################################################################################################
+# Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse
+# of a matrix rather than compute it repeatedly. 
+# This file contains two functions makeCacheMatrix() and cacheSolve() which caches the Inverse of the matrix
+# makeCachefunction () caculates and stores the Inverse of the matrix in the cache. 
+# cachesolve() retreives the cached Inverse value if the matrix has not changed instead of computing it again. 
+# Only if the cache is empty or the matrix has changed then cachesolve() computes the Inverse of the matrix.
 
-## Write a short comment describing this function
+##############################################################################################################################
+
+
+# makeCacheMatrix() caculates and stores the Inverse of the matrix (passed as an argument x) in the cache.
+# It builds a set of functions and returns the functions within a list to the parent environment. 
+# It contains four functions set(), get() , setInverse() & getInverse() and also two data objects, x and m.
 
 makeCacheMatrix <- function(x = matrix()) 
 
@@ -25,7 +35,10 @@ makeCacheMatrix <- function(x = matrix())
 }
 
 
-## Write a short comment describing this function
+## cacheSolve()calculates the Inverse of the matrix created with the above function
+## It first checks if the Inverse of the matrix  (when the matrix has not changed) has alrady been calculated. 
+##If yes then it retrievs the inverse from cache. If not then it calculates the Inverse 
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -46,3 +59,21 @@ cacheSolve <- function(x, ...) {
 
 
 }
+
+########################### Example to test the functions ###########################
+
+# Define a small Matrix
+matr1 <- matrix(1:4, nrow=2, ncol=2)
+
+# myMatrix object prevents the memory consumed by makeCacheMatrix() from being released by the garbage collector.
+myMatrix <- makeCacheMatrix(matr1)
+
+myMatrix$get()
+
+#There is no cached value here so Cachesolve() calculates the Inverse
+cacheSolve(myMatrix)
+
+# Cachesolve() gets the cached value during the second run as the matrix has not changed and cache is not empty
+cacheSolve(myMatrix)
+
+
